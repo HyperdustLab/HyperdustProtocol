1c8ce5e6f880f290619b206736da7e456dfcbc10
fangyeming <fangyemingzz@gmail.com>
2023-10-22 23:38:08 +0800

    Submit missing files

---
+++ contracts/node/Hyperdust_Node_CheckIn.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "../utils/StrUtil.sol";
+
+contract Hyperdust_Node_CheckIn is Ownable {
+    using Strings for *;
+    using StrUtil for *;
+
+    function check(address incomeAddress) public view returns (bool) {
+        return true;
+    }
+}
