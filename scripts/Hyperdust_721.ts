a21e1950916f28049108aa883d40bf2bb3c0b966
fangyeming <fangyeminzz@gmail.com>
2024-03-03 11:53:11 +0800

    Update user authentication logic to use bcrypt for password hashing

---
+++ scripts/Hyperdust_721.ts
@@ -0,0 +1,19 @@
+/** @format */
+
+import { ethers, run } from "hardhat";
+
+async function main() {
+    const contract = await ethers.deployContract("Hyperdust_721", ["Test", "Test"]);
+    await contract.waitForDeployment()
+
+
+
+    console.info("contractFactory address:", contract.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere q
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
