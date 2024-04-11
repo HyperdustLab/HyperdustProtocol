b7ba2258ba2dbb02c52d2de40a1d9e7ea8e8d30e
fangyeming <fangyemingzz@gmail.com>
2024-04-11 13:26:20 +0800

    Fix formatting issues in Test1.ts and Hyperdust_Space.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,107 +1,107 @@
 /** @format */
 require("dotenv").config();
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter";
 import "@openzeppelin/hardhat-upgrades";
 
-const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const {ProxyAgent, setGlobalDispatcher} = require("undici");
 const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
 setGlobalDispatcher(proxyAgent);
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      metadata: {
-        useLiteralContent: true,
-      },
-      optimizer: {
+    solidity: {
+        version: "0.8.20",
+        settings: {
+            metadata: {
+                useLiteralContent: true,
+            },
+            optimizer: {
+                enabled: true,
+                runs: 2000,
+                details: {
+                    yul: true,
+                    yulDetails: {
+                        stackAllocation: true,
+                        optimizerSteps: "dhfoDgvulfnTUtnIf",
+                    },
+                },
+            },
+            viaIR: true,
+        },
+    },
+    networks: {
+        dev: {
+            url: "HTTP://127.0.0.1:8545",
+            loggingEnabled: true,
+            accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY3, process.env.PRIVATE_KEY4],
+        },
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            loggingEnabled: true,
+            accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1],
+        },
+        arbitrumSepolia: {
+            url: process.env.Arbitrum_Sepolia_Testnet_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2],
+            loggingEnabled: true,
+        },
+        arbitrumMainnet: {
+            url: process.env.Arbitrum_Mainnet_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY_PROD],
+            loggingEnabled: true,
+        },
+        optimismSepolia: {
+            url: process.env.Optimism_Sepolia_Testnet_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1],
+            loggingEnabled: true,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.ETHERSCAN_API_KEY,
+            optimismSepolia: process.env.Optimism_Sepolia_KEY,
+            arbitrumSepolia: process.env.Arbitrum_Sepolia_KEY,
+            arbitrumMainnet: process.env.Arbitrum_Mainnet_KEY,
+        },
+        customChains: [
+            {
+                network: "arbitrumSepolia",
+                chainId: 421614,
+                urls: {
+                    apiURL: "https://api-sepolia.arbiscan.io/api",
+                    browserURL: "https://sepolia.arbiscan.io/",
+                },
+            },
+            {
+                network: "arbitrumSepolia",
+                chainId: 421614,
+                urls: {
+                    apiURL: "https://api-sepolia.arbiscan.io/api",
+                    browserURL: "https://sepolia.arbiscan.io/",
+                },
+            },
+            {
+                network: "arbitrumMainnet",
+                chainId: 42161,
+                urls: {
+                    apiURL: "https://api.arbiscan.io/api",
+                    browserURL: "https://arbiscan.io/",
+                },
+            },
+            {
+                network: "optimismSepolia",
+                chainId: 11155420,
+                urls: {
+                    apiURL: "https://api-sepolia-optimistic.etherscan.io/api",
+                    browserURL: "https://sepolia-optimism.etherscan.io/",
+                },
+            },
+        ],
+    },
+    sourcify: {
         enabled: true,
-        runs: 2000,
-        details: {
-          yul: true,
-          yulDetails: {
-            stackAllocation: true,
-            optimizerSteps: "dhfoDgvulfnTUtnIf",
-          },
-        },
-      },
-      viaIR: true,
     },
-  },
-  networks: {
-    dev: {
-      url: "HTTP://127.0.0.1:8545",
-      loggingEnabled: true,
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY3],
-    },
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      loggingEnabled: true,
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1],
-    },
-    arbitrumSepolia: {
-      url: process.env.Arbitrum_Sepolia_Testnet_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2],
-      loggingEnabled: true,
-    },
-    arbitrumMainnet: {
-      url: process.env.Arbitrum_Mainnet_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY_PROD],
-      loggingEnabled: true,
-    },
-    optimismSepolia: {
-      url: process.env.Optimism_Sepolia_Testnet_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY1],
-      loggingEnabled: true,
-    },
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY,
-      optimismSepolia: process.env.Optimism_Sepolia_KEY,
-      arbitrumSepolia: process.env.Arbitrum_Sepolia_KEY,
-      arbitrumMainnet: process.env.Arbitrum_Mainnet_KEY,
-    },
-    customChains: [
-      {
-        network: "arbitrumSepolia",
-        chainId: 421614,
-        urls: {
-          apiURL: "https://api-sepolia.arbiscan.io/api",
-          browserURL: "https://sepolia.arbiscan.io/",
-        },
-      },
-      {
-        network: "arbitrumSepolia",
-        chainId: 421614,
-        urls: {
-          apiURL: "https://api-sepolia.arbiscan.io/api",
-          browserURL: "https://sepolia.arbiscan.io/",
-        },
-      },
-      {
-        network: "arbitrumMainnet",
-        chainId: 42161,
-        urls: {
-          apiURL: "https://api.arbiscan.io/api",
-          browserURL: "https://arbiscan.io/",
-        },
-      },
-      {
-        network: "optimismSepolia",
-        chainId: 11155420,
-        urls: {
-          apiURL: "https://api-sepolia-optimistic.etherscan.io/api",
-          browserURL: "https://sepolia-optimism.etherscan.io/",
-        },
-      },
-    ],
-  },
-  sourcify: {
-    enabled: true,
-  },
 };
 
 export default config;
--- test/Test1.ts
+++ test/Test1.ts
@@ -1,8 +1,8 @@
 /** @format */
 
-import { ethers } from "hardhat";
+import {ethers} from "hardhat";
 
-import { promisify } from "util";
+import {promisify} from "util";
 
 const request = require("request");
 
@@ -13,19 +13,12 @@ const fs = require("fs").promises;
 const requestPromise = promisify(request);
 
 describe("Hyperdust_HYDT_Price", () => {
-  describe("sendRequest", () => {
-    it("sendRequest", async () => {
-      const Hyperdust_Space = await ethers.getContractAt("Hyperdust_Space", "0x0779f2687f0eb4EA70acA2A065eAdE9aFb0F24bF");
+    describe("sendRequest", () => {
+        it("sendRequest", async () => {
+            const accounts = await ethers.getSigners();
 
-      await (
-        await Hyperdust_Space.edit(
-          "0x725e378ad5f187e793c9deda7f03d2cd29a27c6d438e7b1a61cf3762a5550f00",
-          "Advanced Puzzle Constructor",
-          "https://s3.hyperdust.io/upload/2023/12/15/64fb59d2-f34c-4cf9-aeca-e08ef3750b3c.gif",
-          "https://s3.hyperdust.io/upload/2023/12/15/64fb59d2-f34c-4cf9-aeca-e08ef3750b3c.gif",
-          ""
-        )
-      ).wait();
+            console.info(accounts[3].address)
+
+        });
     });
-  });
 });
