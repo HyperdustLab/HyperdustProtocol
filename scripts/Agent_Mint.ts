8c37bef920ff58f859d50b82799c6dd3fde752a3
fangyeming <fangyemimgzz@gmail.com>
2024-06-05 11:20:28 +0800

    Remove unused contracts and dependencies

---
+++ scripts/Agent_Mint.ts
@@ -0,0 +1,21 @@
+/** @format */
+
+import { ethers, run, upgrades } from 'hardhat'
+
+async function main() {
+  const contract = await ethers.getContractFactory('Agent1155Factory')
+  const instance = await upgrades.deployProxy(contract, [process.env.ADMIN_Wallet_Address])
+
+  await instance.waitForDeployment()
+
+  await (await instance.setContractAddress([''])).wait()
+
+  console.info('contractFactory address:', instance.target)
+}
+
+// We recommend this pattern to be able to use async/await everywhere q
+// and properly handle errors.
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
---
+++ scripts/HyperAGI_Agent.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+import { ethers, run, upgrades } from 'hardhat'
+
+async function main() {
+  const _HyperAGI_Storage = await ethers.getContractFactory('HyperAGI_Storage')
+  const HyperAGI_Storage = await upgrades.deployProxy(_HyperAGI_Storage, [process.env.ADMIN_Wallet_Address])
+  await HyperAGI_Storage.waitForDeployment()
+
+  const contract = await ethers.getContractFactory('HyperAGI_Agent')
+  const instance = await upgrades.deployProxy(contract, [process.env.ADMIN_Wallet_Address])
+
+  await instance.waitForDeployment()
+
+  await (await instance.setContractAddress(['0x81fd5C310eE05b96CD2Ff7359ed395b907e357Ff', '0xd6EF21906DC28DA65174E4664D11Ee9Be21C698e', '0x5745090BFB28C3399223215DfbBb4e729aeF8cFD', HyperAGI_Storage.target, ethers.ZeroAddress, '0x4cF4b11E1884483EaC6937f2fBC7D41411776F47'])).wait()
+
+  console.info('contractFactory address:', instance.target)
+}
+
+// We recommend this pattern to be able to use async/await everywhere q
+// and properly handle errors.
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
